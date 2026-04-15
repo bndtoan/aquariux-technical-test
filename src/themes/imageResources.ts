85fcf4ff31f0767aa795f016d12d6484eccd7b97
Toan BND <toanbnd@edoctor.vn>
2026-04-15 20:00:34 +0700

    add image assets

--- src/themes/imageResources.ts
+++ src/themes/imageResources.ts
@@ -1,2 +1,8 @@
 export default {
+  logoMovieDB: require('../assets/logo-moviedb.png'),
+
+  icBack: require('../assets/ic-back.png'),
+  icDelete: require('../assets/ic-delete.png'),
+  icHome: require('../assets/ic-home.png'),
+  icWatchList: require('../assets/ic-watch-list.png'),
 };
